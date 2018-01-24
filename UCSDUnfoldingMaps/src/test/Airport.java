a61709b93e91f180169c3379e2db4225b77d0aa0
CifWang <wcf@nssc.ac.cn>
2018-01-24 19:42:42 +0800

    week6, linear search

---
+++ UCSDUnfoldingMaps/src/test/Airport.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Airport {
+	private String city;
+	private String country;
+	private String code3;
+	
+	public String getCity() {return this.city;}
+	public String getCountry() {return this.country;}
+	public String getCode() {return this.code3;}
+	
+	public static String findAirportCode(String toFind, Airport[] airports) {
+		for(int i=0;i<airports.length;i++) {
+			if(airports[i].getCity().equals(toFind)) {
+				return airports[i].getCity();
+			}
+		}
+		return null;//这个一开始没有，报错，因为如果找不到，就不会返回，所以在返回值那里报错
+	}
+
+}
